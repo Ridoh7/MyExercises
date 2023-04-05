ed795d3a1837010abc5738cd6e3fdfd96b6b6d05
Ridoh Lawal <ridoh.lawal@i-academy.org>
2023-04-05 12:35:28 +0100

    Sample Exercise

---
+++ src/BankAccount.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class BankAccount {
+    public static void main(String[] args) {
+        String firstName; String middleName; String lastName;
+        String accountNumber;
+        String USSD;
+        String pin;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter your First Name");
+        firstName= scanner.nextLine();
+
+        System.out.println("Enter your Middle Name");
+        middleName= scanner.nextLine();
+
+        System.out.println("Enter your Last Name");
+        lastName= scanner.nextLine();
+
+
+        String accountName;
+        accountName=firstName+" "+middleName+" "+lastName;
+
+        System.out.println("Enter your account number");
+        accountNumber=scanner.nextLine();
+
+        System.out.println("Enter your USSD");
+        USSD=scanner.nextLine();
+
+        System.out.println("Enter your pin");
+        pin= scanner.nextLine();
+
+        System.out.printf("""
+                Congrats! Your account has been created successfully
+                Your account name is %S
+                Your account number is %s
+                Your USSD is %s
+                Your account Pin is %s,
+                """,accountName, accountNumber, USSD, pin);
+    }
+}
