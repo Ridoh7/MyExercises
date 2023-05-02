fa79b7ee06a81ba63da51b5b86537b093c0df789
Ridoh.Lawal <ridohlawal96@gmail.com>
2023-05-02 09:11:49 +0100

    i-academy.java

---
+++ src/Abstraction/SalariedEmployee.java
@@ -0,0 +1,44 @@
+package Abstraction;
+
+public class SalariedEmployee extends Employee{
+
+    private double weeklySalary;
+
+    public SalariedEmployee(String firstName, String lastName, String socialSecurityNumber,
+                            double weeklySalary) {
+        super(firstName, lastName,socialSecurityNumber);
+        if (weeklySalary < 0) {
+            throw new IllegalArgumentException("Weekly salary cannot be negative");
+        }
+        this.weeklySalary = weeklySalary;
+    }
+
+    public double getWeeklySalary() {
+        return weeklySalary;
+    }
+
+    public void setWeeklySalary(double weeklySalary) {
+        if (weeklySalary < 0) {
+            throw new IllegalArgumentException("Weekly salary cannot be negative");
+        }
+        this.weeklySalary = weeklySalary;
+    }
+
+    @Override
+    public String toString() {
+        return "SalariedEmployee{" + super.toString() +
+                "weeklySalary=" + weeklySalary +
+                '}';
+    }
+
+    @Override
+    public double getPaymentAmount() {
+        return getWeeklySalary();
+    }
+
+//    @Override
+//    public double earnings() {
+//        return getWeeklySalary();
+//    }
+}
+
